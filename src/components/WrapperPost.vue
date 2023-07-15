22618b8399290f56f014b89402815623bb811bb4
longxiang.li <lilongxiang2000@gmail.com>
2023-07-15 19:20:51 +0800

    feat: a 替换为 RouterLink

--- src/components/WrapperPost.vue
+++ src/components/WrapperPost.vue
@@ -7,6 +7,6 @@ const router = useRouter()
 <template>
   <div v-if="route.path !== '/'" my-10 font-mono text-sm>
     <span op50>{{ '>' }}&nbsp;</span>
-    <RouterLink v-text="'cd ..'" underline op50 hover:op100 :to="''" @click="router.back()" />
+    <RouterLink v-text="'cd ..'" underline op50 hover:op100 :to="''" @click.prevent="router.back()" />
   </div>
 </template>
