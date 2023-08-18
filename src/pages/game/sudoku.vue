8689455c58b4858c00da45a84c2c4f5fbc7b2a53
longxiang.li <lilongxiang2000@gmail.com>
2023-08-18 23:17:10 +0800

    init: 数独

---
+++ src/pages/game/sudoku.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="xy-center mx-auto">
+    <div class="b-1 b-gray b-solid">
+      <div
+        v-for="i in 9"
+        class="flex"
+      >
+        <div
+          v-for="j in 9"
+          class="cursor-pointer w-8 h-8 b-1 b-gray b-solid text-base hover:bg-gray xy-center"
+        >
+          {{ 9 * (i - 1) + j }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
