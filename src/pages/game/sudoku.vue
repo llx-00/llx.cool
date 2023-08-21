8dbf7be5f032cc20fd5daf4cd0d6532e42def2ad
longxiang.li <lilongxiang2000@gmail.com>
2023-08-21 09:49:41 +0800

    feat: hover时，块、行、列样式

--- src/pages/game/sudoku.vue
+++ src/pages/game/sudoku.vue
@@ -64,90 +64,93 @@
   }
 
   const initVal = generateSudokuBoard();
+  devLog("initVal", initVal);
   const initMap = reactive(initVal.map);
   const initAns = reactive(initVal.ans);
 
   function handleClick(i: number, j: number) {
-    const clickItem = initMap[i - 1][j - 1];
-    if (import.meta.env) {
-      devLog(`(${i},${j}):`, clickItem);
-    }
+    const clickItem = initMap[i][j];
+    devLog(`(${i},${j}):`, clickItem);
   }
 </script>
 
 <template>
   <div class="xy-center mx-auto select-none text-base">
-    <div class="b-2 b-gray-500 dark:b-gray-400 b-solid">
-      <div
-        v-for="i in 9"
-        class="board-row flex"
+    <table class="border-collapse font-mono">
+      <colgroup
+        v-for="i in 3"
+        class="b-3 b-gray-500 b-solid dark:b-gray-400"
       >
-        <div
-          v-for="j in 9"
-          class="board-cell cursor-pointer w-8 h-8 b-1 b-gray-500 dark:b-gray-400 b-solid xy-center"
-          :class="[
-            i === 3 || i === 6 ? 'b-b-2' : null,
-            i === 4 || i === 7 ? 'b-t-2' : null,
-            j === 3 || j === 6 ? 'b-r-2' : null,
-            j === 4 || j === 7 ? 'b-l-2' : null,
-          ]"
-          @click="handleClick(i, j)"
+        <col v-for="j in 3" />
+      </colgroup>
+      <tbody
+        v-for="i in 3"
+        class="b-3 b-gray-500 b-solid dark:b-gray-400"
+      >
+        <tr
+          v-for="j in 3"
+          class="hover:bg-yellow"
         >
-          {{ initMap[i - 1][j - 1] }}
-        </div>
-      </div>
-    </div>
+          <td
+            v-for="k in 9"
+            class="w-8 h-8 b-1 b-gray-500 b-solid dark:b-gray-400 text-center"
+            @click="handleClick((i - 1) * 3 + (j - 1), k - 1)"
+          >
+            <!-- {{ `${i},${j},${k}` }} -->
+            {{ initMap[(i - 1) * 3 + (j - 1)][k - 1] }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <style scoped lang="scss">
-  .board-row:has(.board-cell:hover) {
-    background-color: rgba(250, 204, 21, var(--un-text-opacity));
-  }
+  table {
+    // 选中所在列
+    &:has(td:nth-child(1):hover) td:nth-child(1),
+    &:has(td:nth-child(2):hover) td:nth-child(2),
+    &:has(td:nth-child(3):hover) td:nth-child(3),
+    &:has(td:nth-child(4):hover) td:nth-child(4),
+    &:has(td:nth-child(5):hover) td:nth-child(5),
+    &:has(td:nth-child(6):hover) td:nth-child(6),
+    &:has(td:nth-child(7):hover) td:nth-child(7),
+    &:has(td:nth-child(8):hover) td:nth-child(8),
+    &:has(td:nth-child(9):hover) td:nth-child(9) {
+      --un-bg-opacity: 1;
+      background-color: rgba(250, 204, 21, var(--un-bg-opacity));
+    }
 
-  table:has(tbody:nth-of-type(1) td:nth-child(n + 1):nth-child(-n + 3):hover)
-    tbody:nth-of-type(1)
-    td:nth-child(n + 1):nth-child(-n + 3),
-  table:has(tbody:nth-of-type(1) td:nth-child(n + 4):nth-child(-n + 6):hover)
-    tbody:nth-of-type(1)
-    td:nth-child(n + 4):nth-child(-n + 6),
-  table:has(tbody:nth-of-type(1) td:nth-child(n + 7):nth-child(-n + 9):hover)
-    tbody:nth-of-type(1)
-    td:nth-child(n + 7):nth-child(-n + 9),
-  table:has(tbody:nth-of-type(2) td:nth-child(n + 1):nth-child(-n + 3):hover)
-    tbody:nth-of-type(2)
-    td:nth-child(n + 1):nth-child(-n + 3),
-  table:has(tbody:nth-of-type(2) td:nth-child(n + 4):nth-child(-n + 6):hover)
-    tbody:nth-of-type(2)
-    td:nth-child(n + 4):nth-child(-n + 6),
-  table:has(tbody:nth-of-type(2) td:nth-child(n + 7):nth-child(-n + 9):hover)
-    tbody:nth-of-type(2)
-    td:nth-child(n + 7):nth-child(-n + 9),
-  table:has(tbody:nth-of-type(3) td:nth-child(n + 1):nth-child(-n + 3):hover)
-    tbody:nth-of-type(3)
-    td:nth-child(n + 1):nth-child(-n + 3),
-  table:has(tbody:nth-of-type(3) td:nth-child(n + 4):nth-child(-n + 6):hover)
-    tbody:nth-of-type(3)
-    td:nth-child(n + 4):nth-child(-n + 6),
-  table:has(tbody:nth-of-type(3) td:nth-child(n + 7):nth-child(-n + 9):hover)
-    tbody:nth-of-type(3)
-    td:nth-child(n + 7):nth-child(-n + 9) {
-    background-color: yellow;
-  }
-
-  table:has(td:nth-child(1):hover) td:nth-child(1),
-  table:has(td:nth-child(2):hover) td:nth-child(2),
-  table:has(td:nth-child(3):hover) td:nth-child(3),
-  table:has(td:nth-child(4):hover) td:nth-child(4),
-  table:has(td:nth-child(5):hover) td:nth-child(5),
-  table:has(td:nth-child(6):hover) td:nth-child(6),
-  table:has(td:nth-child(7):hover) td:nth-child(7),
-  table:has(td:nth-child(8):hover) td:nth-child(8),
-  table:has(td:nth-child(9):hover) td:nth-child(9) {
-    background-color: yellow;
+    // 选中所在的3*3
+    &:has(tbody:nth-of-type(1) td:nth-child(n + 1):nth-child(-n + 3):hover)
+      tbody:nth-of-type(1)
+      td:nth-child(n + 1):nth-child(-n + 3),
+    &:has(tbody:nth-of-type(1) td:nth-child(n + 4):nth-child(-n + 6):hover)
+      tbody:nth-of-type(1)
+      td:nth-child(n + 4):nth-child(-n + 6),
+    &:has(tbody:nth-of-type(1) td:nth-child(n + 7):nth-child(-n + 9):hover)
+      tbody:nth-of-type(1)
+      td:nth-child(n + 7):nth-child(-n + 9),
+    &:has(tbody:nth-of-type(2) td:nth-child(n + 1):nth-child(-n + 3):hover)
+      tbody:nth-of-type(2)
+      td:nth-child(n + 1):nth-child(-n + 3),
+    &:has(tbody:nth-of-type(2) td:nth-child(n + 4):nth-child(-n + 6):hover)
+      tbody:nth-of-type(2)
+      td:nth-child(n + 4):nth-child(-n + 6),
+    &:has(tbody:nth-of-type(2) td:nth-child(n + 7):nth-child(-n + 9):hover)
+      tbody:nth-of-type(2)
+      td:nth-child(n + 7):nth-child(-n + 9),
+    &:has(tbody:nth-of-type(3) td:nth-child(n + 1):nth-child(-n + 3):hover)
+      tbody:nth-of-type(3)
+      td:nth-child(n + 1):nth-child(-n + 3),
+    &:has(tbody:nth-of-type(3) td:nth-child(n + 4):nth-child(-n + 6):hover)
+      tbody:nth-of-type(3)
+      td:nth-child(n + 4):nth-child(-n + 6),
+    &:has(tbody:nth-of-type(3) td:nth-child(n + 7):nth-child(-n + 9):hover)
+      tbody:nth-of-type(3)
+      td:nth-child(n + 7):nth-child(-n + 9) {
+      --un-bg-opacity: 1;
+      background-color: rgba(250, 204, 21, var(--un-bg-opacity));
+    }
   }
 </style>
-
-<!--
-  - hover 样式
- -->
