d6b94d1a0be6db5603c99f19b910e08c5f5a1e67
longxiang.li <lilongxiang2000@gmail.com>
2023-08-31 22:51:37 +0800

    chore: add `naive-ui`, `highlight.js`

--- src/pages/project/box-shadows.vue
+++ src/pages/project/box-shadows.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
-  import { StyleValue } from "vue";
+  import { StyleValue } from "vue"
+  import { NConfigProvider, NCode, useMessage } from "naive-ui"
+  import { createTheme, codeDark } from "naive-ui"
+  import { zhCN, dateZhCN } from "naive-ui"
+  import highlight from "highlight.js"
+  import hljsCSS from "highlight.js/lib/languages/css"
+
+  highlight.registerLanguage("javascript", hljsCSS)
+
+  const darkTheme = createTheme([codeDark])
 
   useHead({
     title: "Preview Box Shadows",
-  });
+  })
 
-  const refBox = ref();
+  const refBox = ref()
 
   const initStyle: StyleValue = {
     boxShadow: `
@@ -13,70 +22,83 @@
       0 0 0 2px rgb(255, 255, 255),
       0.3em 0.3em 1em rgba(0, 0, 0, 0.3)
     `,
-  };
+  }
 
-  const inputColor = ref("");
+  const inputColor = ref("")
   const inputStyle = computed(() => {
     return {
       boxShadow: `5px 5px 5px ${inputColor.value || "rgba(0, 0, 0, 0.1)"};`,
-    };
-  });
+    }
+  })
 
-  const showCode = ref(false);
+  const showCode = ref(false)
 </script>
 
 <template>
-  <h1>施工中</h1>
-  <div class="flex justify-between flex-wrap gap-4 text-base">
-    <div
-      class="b-solid b-gray rd-1 flex-1 min-w-300px w-100% flex flex-col gap-2 p-2 overflow-hidden"
-    >
-      <div class="w-100% flex justify-end items-center">
-        <i
-          :class="showCode ? 'i-lucide-code-2' : 'i-lucide-code'"
-          @click="showCode = !showCode"
-        />
+  <NConfigProvider
+    :theme="isDark ? darkTheme : null"
+    :locale="zhCN"
+    :date-locale="dateZhCN"
+    :hljs="highlight"
+  >
+    <h1>施工中</h1>
+    <div class="flex justify-between flex-wrap gap-4 text-base">
+      <div
+        class="b-solid b-gray rd-1 flex-1 min-w-300px w-100% flex flex-col gap-2 p-2 overflow-hidden"
+      >
+        <div class="w-100% flex justify-end items-center gap-2">
+          <i
+            class="i-lucide-copy cursor-pointer"
+            @click=""
+          />
+          <i
+            class="cursor-pointer"
+            :class="showCode ? 'i-lucide-code-2' : 'i-lucide-code'"
+            @click="showCode = !showCode"
+          />
+        </div>
+        <div class="flex-1 min-h-240px xy-center">
+          <div
+            v-if="!showCode"
+            ref="refBox"
+            class="rd-1 w-100px h-100px"
+            :style="initStyle"
+          />
+          <NCode
+            v-else
+            class="h-100% m-0 p-0 text-sm overflow-x-auto select-none"
+            language="css"
+            :code="JSON.stringify(inputStyle, null, 2)"
+          />
+        </div>
       </div>
-      <div class="flex-1 min-h-240px xy-center">
-        <div
-          v-if="!showCode"
-          ref="refBox"
-          class="rd-1 w-100px h-100px"
-          :style="initStyle"
-        />
-        <pre
-          v-else
-          class="h-100% m-0 p-0 text-sm overflow-x-auto"
-          >{{ JSON.stringify(inputStyle, null, 2) }}</pre
-        >
+      <div
+        class="b-solid b-gray rd-1 flex-1 min-w-300px min-h-240px w-100% flex flex-col p-2"
+      >
+        <div>
+          <span class="min-w-30% inline-block">阴影颜色</span>
+          <input
+            type="color"
+            v-model="inputColor"
+          />
+        </div>
+        <div>
+          <span class="min-w-30% inline-block">X轴偏移量</span>
+          <input
+            type="number"
+            class="w-20%"
+          />
+          px
+        </div>
+        <div>
+          <span class="min-w-30% inline-block">Y轴偏移量</span>
+          <input
+            type="number"
+            class="w-20%"
+          />
+          px
+        </div>
       </div>
     </div>
-    <div
-      class="b-solid b-gray rd-1 flex-1 min-w-300px min-h-240px w-100% flex flex-col p-2"
-    >
-      <div>
-        <span class="min-w-30% inline-block">阴影颜色</span>
-        <input
-          type="color"
-          v-model="inputColor"
-        />
-      </div>
-      <div>
-        <span class="min-w-30% inline-block">X轴偏移量</span>
-        <input
-          type="number"
-          class="w-20%"
-        />
-        px
-      </div>
-      <div>
-        <span class="min-w-30% inline-block">Y轴偏移量</span>
-        <input
-          type="number"
-          class="w-20%"
-        />
-        px
-      </div>
-    </div>
-  </div>
+  </NConfigProvider>
 </template>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,9 @@
-import dayjs from "dayjs";
-import lodash from "lodash";
+import dayjs from "dayjs"
+import lodash from "lodash"
 
 export function formatDate(d: string | Date) {
-  const date = dayjs(d);
-  return date.format("YYYY/MM/DD");
+  const date = dayjs(d)
+  return date.format("YYYY/MM/DD")
 }
 
 /**
@@ -15,7 +15,7 @@ export function getMatrix(): number[][] {
     lodash.shuffle(
       [getRandom2or4(), getRandom2or4()].concat(new Array(14).fill(0))
     )
-  )!;
+  )!
 }
 
 /**
@@ -23,7 +23,7 @@ export function getMatrix(): number[][] {
  * return Math.random() < 0.9 ? 2 : 4
  */
 export function getRandom2or4() {
-  return Math.random() < 0.9 ? 2 : 4;
+  return Math.random() < 0.9 ? 2 : 4
 }
 
 /**
@@ -33,15 +33,15 @@ export function getRandom2or4() {
  */
 export function Adto2d<T>(arr: T[]) {
   const n = arr.length,
-    sqrt_n = Math.sqrt(arr.length);
+    sqrt_n = Math.sqrt(arr.length)
 
-  if (Math.sqrt(n) ** 2 !== n) return null;
+  if (Math.sqrt(n) ** 2 !== n) return null
 
-  const newArr: T[][] = [];
+  const newArr: T[][] = []
   for (let i = 0; i < sqrt_n; i++) {
-    newArr.push(arr.slice(sqrt_n * i, sqrt_n * (i + 1)));
+    newArr.push(arr.slice(sqrt_n * i, sqrt_n * (i + 1)))
   }
-  return newArr;
+  return newArr
 }
 
 /**
@@ -51,27 +51,27 @@ export function Adto2d<T>(arr: T[]) {
  * @returns new Arr
  */
 export function rotateMatrix<T>(matrix: T[][]) {
-  const _matrix = lodash.cloneDeep(matrix);
-  const n = matrix.length;
+  const _matrix = lodash.cloneDeep(matrix)
+  const n = matrix.length
 
   // 沿对角线镜像对称二维矩阵
   for (let i = 0; i < n; i++) {
     for (let j = i; j < n; j++) {
-      [_matrix[i][j], _matrix[j][i]] = [_matrix[j][i], _matrix[i][j]];
+      ;[_matrix[i][j], _matrix[j][i]] = [_matrix[j][i], _matrix[i][j]]
     }
   }
 
   function reverseRow(arr: T[]) {
     let i = 0,
-      j = arr.length - 1;
+      j = arr.length - 1
     while (i <= j) {
-      [arr[i++], arr[j--]] = [arr[j], arr[i]];
+      ;[arr[i++], arr[j--]] = [arr[j], arr[i]]
     }
   }
 
-  for (let row of _matrix) reverseRow(row);
+  for (let row of _matrix) reverseRow(row)
 
-  return _matrix;
+  return _matrix
 }
 
 /**
@@ -79,6 +79,13 @@ export function rotateMatrix<T>(matrix: T[][]) {
  */
 export function devLog(...arg: any[]) {
   if (import.meta.env.DEV) {
-    console.log(...arg);
+    console.log(...arg)
   }
+}
+
+/**
+ * 写入粘贴板文字
+ */
+export function writeClipboardText(text: string) {
+  return navigator?.clipboard.writeText(text)
 }
