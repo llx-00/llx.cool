1e1d0e920707613f6ee62db3321fe650a6725c31
longxiang.li <lilongxiang2000@gmail.com>
2023-09-01 16:47:38 +0800

    fix: build err 未解决

--- src/pages/project/box-shadows.vue
+++ src/pages/project/box-shadows.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
   import { StyleValue } from "vue"
-  import { NConfigProvider, NCode, useMessage } from "naive-ui"
-  import { createTheme, codeDark } from "naive-ui"
-  import { zhCN, dateZhCN } from "naive-ui"
+  // import { createTheme, codeDark, useMessage, zhCN, dateZhCN } from "naive-ui"
+  import { NConfigProvider } from "naive-ui"
+  // import { NConfigProvider, NCode } from "naive-ui"
   import highlight from "highlight.js"
   import hljsCSS from "highlight.js/lib/languages/css"
 
-  highlight.registerLanguage("javascript", hljsCSS)
+  highlight.registerLanguage("css", hljsCSS)
 
-  const darkTheme = createTheme([codeDark])
+  // const darkTheme = createTheme([codeDark])
 
   useHead({
     title: "Preview Box Shadows",
@@ -35,70 +35,74 @@
 </script>
 
 <template>
-  <NConfigProvider
+  <!-- <NConfigProvider
     :theme="isDark ? darkTheme : null"
     :locale="zhCN"
     :date-locale="dateZhCN"
     :hljs="highlight"
-  >
-    <h1>施工中</h1>
-    <div class="flex justify-between flex-wrap gap-4 text-base">
-      <div
-        class="b-solid b-gray rd-1 flex-1 min-w-300px w-100% flex flex-col gap-2 p-2 overflow-hidden"
-      >
-        <div class="w-100% flex justify-end items-center gap-2">
-          <i
-            class="i-lucide-copy cursor-pointer"
-            @click=""
-          />
-          <i
-            class="cursor-pointer"
-            :class="showCode ? 'i-lucide-code-2' : 'i-lucide-code'"
-            @click="showCode = !showCode"
-          />
-        </div>
-        <div class="flex-1 min-h-240px xy-center">
-          <div
-            v-if="!showCode"
-            ref="refBox"
-            class="rd-1 w-100px h-100px"
-            :style="initStyle"
-          />
-          <NCode
-            v-else
-            class="h-100% m-0 p-0 text-sm overflow-x-auto select-none"
-            language="css"
-            :code="JSON.stringify(inputStyle, null, 2)"
-          />
-        </div>
+  > -->
+  <h1>施工中</h1>
+  <div class="flex justify-between flex-wrap gap-4 text-base">
+    <div
+      class="b-solid b-gray rd-1 flex-1 min-w-300px w-100% flex flex-col gap-2 p-2 overflow-hidden"
+    >
+      <div class="w-100% flex justify-end items-center gap-2">
+        <i
+          class="i-lucide-copy cursor-pointer"
+          @click=""
+        />
+        <i
+          class="cursor-pointer"
+          :class="showCode ? 'i-lucide-code-2' : 'i-lucide-code'"
+          @click="showCode = !showCode"
+        />
       </div>
       <div
-        class="b-solid b-gray rd-1 flex-1 min-w-300px min-h-240px w-100% flex flex-col p-2"
+        class="flex-1 min-h-240px p-2 rd-1 xy-center"
+        :class="[showCode ? 'bg-gray-200 dark:bg-gray-800' : null]"
       >
-        <div>
-          <span class="min-w-30% inline-block">阴影颜色</span>
-          <input
-            type="color"
-            v-model="inputColor"
-          />
-        </div>
-        <div>
-          <span class="min-w-30% inline-block">X轴偏移量</span>
-          <input
-            type="number"
-            class="w-20%"
-          />
-          px
-        </div>
-        <div>
-          <span class="min-w-30% inline-block">Y轴偏移量</span>
-          <input
-            type="number"
-            class="w-20%"
-          />
-          px
-        </div>
+        <div
+          v-if="!showCode"
+          ref="refBox"
+          class="rd-1 w-100px h-100px"
+          :style="initStyle"
+        />
+        <!-- <NCode
+            v-else
+            class="w-100% h-100% text-xs overflow-x-auto select-none"
+            language="css"
+            trim
+            :code="`boxShadow: ${initStyle.boxShadow}`"
+          /> -->
       </div>
     </div>
-  </NConfigProvider>
+    <div
+      class="b-solid b-gray rd-1 flex-1 min-w-300px min-h-240px w-100% flex flex-col p-2"
+    >
+      <div>
+        <span class="min-w-30% inline-block">阴影颜色</span>
+        <input
+          type="color"
+          v-model="inputColor"
+        />
+      </div>
+      <div>
+        <span class="min-w-30% inline-block">X轴偏移量</span>
+        <input
+          type="number"
+          class="w-20%"
+        />
+        px
+      </div>
+      <div>
+        <span class="min-w-30% inline-block">Y轴偏移量</span>
+        <input
+          type="number"
+          class="w-20%"
+        />
+        px
+      </div>
+    </div>
+  </div>
+  <!-- </NConfigProvider> -->
 </template>
