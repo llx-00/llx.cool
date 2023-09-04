dbf64cc84433f08289dd1ca108eaa28768a48855
longxiang.li <lilongxiang2000@gmail.com>
2023-09-04 22:50:28 +0800

    feat: 虚拟进度条

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import "./styles/markdown.scss"
 
 import { ViteSSG } from "vite-ssg"
 import autoRoutes from "pages-generated"
+import NProgress from "nprogress"
+import { setupRouterScroller } from "vue-router-better-scroller"
+
 import App from "./App.vue"
 
 const routes = autoRoutes.map(i => {
@@ -12,4 +15,30 @@ const routes = autoRoutes.map(i => {
   }
 })
 
-export const createApp = ViteSSG(App, { routes })
+export const createApp = ViteSSG(
+  App,
+  { routes },
+  ({ router, app, isClient }) => {
+    if (isClient) {
+      const html = document.querySelector("html")!
+      setupRouterScroller(router, {
+        selectors: {
+          html(ctx) {
+            // only do the sliding transition when the scroll position is not 0
+            if (ctx.savedPosition?.top) html.classList.add("no-sliding")
+            else html.classList.remove("no-sliding")
+            return true
+          },
+        },
+        behavior: "auto",
+      })
+
+      router.beforeEach(() => {
+        NProgress.start()
+      })
+      router.afterEach(() => {
+        NProgress.done()
+      })
+    }
+  }
+)
