16ed7114e437a9ecac71b85d8b805a978980a8a8
longxiang.li <lilongxiang2000@gmail.com>
2023-09-25 11:00:16 +0800

    perf: 样式优化

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -3,17 +3,15 @@
 </script>
 
 <template>
-  <h1>
-    <div class="mr-2">
-      {{ meta.frontmatter.display || meta.frontmatter.title }}
-    </div>
-    <div
-      v-if="meta.frontmatter.date"
-      class="text-base ws-nowrap font-normal"
-    >
-      {{ formatDate(meta.frontmatter.date) }}
-    </div>
+  <h1 class="mb-0">
+    {{ meta.frontmatter.display || meta.frontmatter.title }}
   </h1>
+  <div
+    v-if="meta.frontmatter.date"
+    class="text-base ws-nowrap font-normal"
+  >
+    {{ formatDate(meta.frontmatter.date) }}
+  </div>
   <p class="font-italic text-base mb-10">
     {{ meta.frontmatter.subtitle }}
   </p>
