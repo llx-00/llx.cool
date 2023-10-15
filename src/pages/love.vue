8e8a31992e3548318513d0172db98f0dc2183fa8
longxiang.li <lilongxiang2000@gmail.com>
2023-10-15 22:21:57 +0800

    feat: `TARGET_TIME`

--- src/pages/love.vue
+++ src/pages/love.vue
@@ -6,7 +6,7 @@
 
   const show = ref(false)
 
-  const TARGET_TIME = "2023/10/13 21:00:00"
+  const TARGET_TIME = "2023/10/27 21:00:00"
 
   function getDiffTime(targetTime: string) {
     const _targetTime = dayjs(targetTime)
